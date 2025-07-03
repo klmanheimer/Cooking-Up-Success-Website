7cc6f4e7745900773ea1f1362bebb43751af07df
klmanheimer <man23048@byui.edu>
2025-07-03 13:54:59 -0600

    mistake
    Co-authored-by: Prashab Gharti Chetri <pchetri8888@gmail.com>
    Co-authored-by: jbecker05 <jbecker05@users.noreply.github.com>
    Co-authored-by: DanService28 <DanService28@users.noreply.github.com>

--- .js
+++
@@ -1,9 +0,0 @@
- const check = document.getElementById('check');
-    check.addEventListener('change', function() {
-        const navMobile = document.querySelector('.nav-mobile');
-        if (this.checked) {
-            navMobile.style.display = 'block';
-        } else {
-            navMobile.style.display = 'none';
-        }
-    });
